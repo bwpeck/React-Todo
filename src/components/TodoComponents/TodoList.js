8bf0a2e66d2421514b765029da4460fe8d144242
bwpeck <benjaminwade483@yahoo.com>
2019-05-08 17:12:55 -0400

    component js done

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+import Todo from './Todo';
+import { SSL_OP_TLS_BLOCK_PADDING_BUG } from 'constants';
+
+const TodoList = props => {
+    return (
+        <div>
+            {props.todos.map(todo => (
+                <Todo
+                handleToggleComplete={props.handleToggleComplete}
+                key={todo.id}
+                todo={todo}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default TodoList;
